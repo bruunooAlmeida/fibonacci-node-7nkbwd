e0321bb742f0832c3159f22caae8ab93bdf91c8c
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-02-13 22:58:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fibonacci-node-7nkbwd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fibonacci-node-7nkbwd)
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * Desafio: escrever uma função que receba um numero, e retorne o valor deste numero na sequencia de Fibonacci
+ * A sequencia de Fibonacci é a sequencia de números onde cada termo corresponde a soma dos dois anteriores
+ * Mais detalhes em: https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci
+ *
+ * Voce pode testar o seu codigo rodando o comando `npm run test` no terminal
+ * e tambem pode alterar o arquivo `index.test.js` se desejar.
+ * Apos enviado, seu codigo sera validado com outros cenarios de teste tambem.
+ *
+ * @example `fibonacci(4)` retorna `5`
+ * @param n numero da sequencia desejada (1 <= n <= 50)
+ * @returns valor da sequencia
+ */
+function fibonacci(n) {
+  if (value < 2) {
+    return 1;
+  } else return fibonacci(value - 1) + fibonacci(value - 2);
+}
